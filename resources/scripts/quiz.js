83faa8ed4887c462ad4dc8b45174f02e2d2ea43f
Keshav Mahawar <kes.mahawar@gmail.com>
2020-06-06 23:56:07 +0530

    quiz DOM and quiz Controller connected

--- resources/scripts/quiz.js
+++ resources/scripts/quiz.js
@@ -1,3 +1,4 @@
+var wrapper
 var quizController = function( qu ){
     var quiz = qu
     checkState()
@@ -13,6 +14,7 @@ var quizController = function( qu ){
     function submitAnswer( optionNo ){
         var question = currentQuestion()
         question.recordedAnswer = optionNo
+        console.log( question )
         incrementState()
         return question.correctAnswer
     }
@@ -23,8 +25,8 @@ var quizController = function( qu ){
 
     function parseQuestionForUser( ques ){
         return {
-                question: ques.text,
-                option: ques.options
+                questionText: ques.text,
+                options: ques.options
                 }
     }
 
@@ -39,7 +41,7 @@ var quizController = function( qu ){
             window.location = 'result.html'
         }
     }
-    return{ getNextQuestion, getCurrentQuestion, submitAnswer, checkState}
+    return{ getNextQuestion, getCurrentQuestion, submitAnswer}
 }
 function init(){
     var storage = localStorage.getItem('quiz')
@@ -51,9 +53,51 @@ function init(){
     
     console.log( JSON.parse(storage) )
     quizController = quizController( JSON.parse(storage) )
+    setQuestion()
     
 }
-init()
-console.log( quizController.getNextQuestion() )
+
+function submitOption(){
+    var target = event.target
+    if( target.className == "option"){
+        var selectedOption = Number( target.id )
+        var correctOption = quizController.submitAnswer( selectedOption )
+        console.log(selectedOption, correctOption)
+        if ( correctOption == selectedOption ){
+            target.style.background = "green"
+        }
+        else{
+            target.style.background = "red"
+        }
+        setTimeout(setQuestion,1500)
+    }
+}
+function setQuestion(){
+        
+    var question = quizController.getCurrentQuestion()
+
+    wrapper.textContent = ""
+    wrapper.append( createQuizDom(question) )
+
+}
+function createQuizDom( question ){
+    var mainDiv = document.createElement('div')
+    var questionDiv = document.createElement('div')
+    questionDiv = question.questionText
+
+    mainDiv.append( questionDiv )
+    question.options.forEach(function( element, index ){
+        var optionDiv = document.createElement('div')
+        optionDiv.textContent = element
+        optionDiv.id = index
+        optionDiv.className = "option"
+        mainDiv.append( optionDiv )
+    })
+    return mainDiv
+}
+console.log( quizController )
 window.addEventListener('load',function(){
+    wrapper = document.querySelector('.wrapper')
+    wrapper.addEventListener( 'click', submitOption ) 
+    init()
 })
